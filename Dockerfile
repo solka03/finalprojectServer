af021d67893ca2b0fe7f60fae5a48dc71bd1bec4
kamlesh <kamlesh.ldce@gmail.com>
2017-04-20 11:41:25 +0530

    commit the server binary file

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 FROM ubuntu:14.04
 
 # tell the port number the container should expose
-EXPOSE 50055
+EXPOSE 9090
 
 RUN mkdir -p /go/src/app/uuid-server
 
--- server/main.go
+++ server/main.go
@@ -4,17 +4,21 @@ import (
 	"encoding/json"
 	pb "finalprojectServer/generator"
 	"fmt"
+	uuid "github.com/satori/go.uuid"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"log"
+	//"net"
+
 	"net"
-	"os/exec"
+	"net/http"
+	"strings"
 	"sync"
 )
 
 const (
-	port = ":50055"
+	port = ":9090"
 )
 
 // server is used to implement helloworld.GreeterServer.
@@ -36,16 +40,14 @@ var sum = 0
 
 // SayHello implements helloworld.GreeterServer
 func (s *server) GenerateUUIDs(ctx context.Context, in *pb.RequireUuidCount) (*pb.UuidString, error) {
-
+	fmt.Println("Inside GenerateUUIDs")
 	genaretedUUIds := make([]string, int(in.GetCount()))
 
 	for i := 0; i < int(in.GetCount()); i++ {
-		out, err := exec.Command("uuidgen").Output()
-		if err != nil {
-			log.Fatal(err)
-		}
+
+		out := uuid.NewV4()
 		fmt.Printf("%s", out)
-		genaretedUUIds[i] = string(out)
+		genaretedUUIds[i] = out.String()
 		mu.Lock()
 		sum = sum + 1
 
@@ -61,6 +63,7 @@ func (s *server) GenerateUUIDs(ctx context.Context, in *pb.RequireUuidCount) (*p
 }
 
 func main() {
+	fmt.Println("Starting Server")
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -72,4 +75,23 @@ func main() {
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+
+	//http.HandleFunc("/", sayhelloName)       // set router
+	//err := http.ListenAndServe(":9090", nil) // set listen port
+	//if err != nil {
+	//	log.Fatal("ListenAndServe: ", err)
+	//}
+}
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()       // parse arguments, you have to call this by yourself
+	fmt.Println(r.Form) // print form information in server side
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello astaxie!") // send data to client side
 }
